7e396490efcaf81015584578b08dea8e4a8e736f
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-03-29 16:43:13 -0400

    added HtmlWebpackPlugin to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My CH5 Project'
+        })
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource'
-            }
-        ]
     }
 }
