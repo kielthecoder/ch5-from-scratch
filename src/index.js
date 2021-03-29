1b20a34f5234a0ad24d60c41726021728bde37e6
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-03-29 16:04:08 -0400

    managing css assets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'World'], ' ');
+    element.classList.add('hello');
 
     return element;
 }
