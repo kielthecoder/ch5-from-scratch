921b29578aee41067a722ed263e27014aafc6696
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-03-29 16:57:04 -0400

    added a watch script

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'My CH5 Project'
         })
     ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     }
