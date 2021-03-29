1a4c9edd8aab00f159a50f90e2fc86e8e9a4cf02
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-03-29 16:45:44 -0400

    removed css from index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-
-import CrestronLogo from './crestron.png';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'World'], ' ');
-    element.classList.add('hello');
-
-    const logo = new Image();
-    logo.src = CrestronLogo;
-
-    element.appendChild(logo);
 
     return element;
 }
