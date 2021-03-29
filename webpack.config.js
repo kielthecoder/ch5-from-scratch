f010aefce6b34a075c77921808505ef13ae5c314
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-03-29 16:59:54 -0400

    added a serve script

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         index: './src/index.js',
         print: './src/print.js'
     },
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'My CH5 Project'
